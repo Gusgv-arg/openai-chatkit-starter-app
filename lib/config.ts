d642fa658d0d4d15ab231b04f827716c474c5456
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-11-03 17:11:32 -0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -13,9 +13,9 @@ export const STARTER_PROMPTS: StartScreenPrompt[] = [
   },
 ];
 
-export const PLACEHOLDER_INPUT = "Ask anything...";
+export const PLACEHOLDER_INPUT = "Hacé tu pregunta...";
 
-export const GREETING = "How can I help you today?";
+export const GREETING = "¿Cómo te puedo ayudar?";
 
 export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
   color: {
